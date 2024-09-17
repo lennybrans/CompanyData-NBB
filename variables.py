6bf4406f9a0dcbc8d01035a3b15b1e19c5af522e
lennybrans <len_3000@outlook.com>
2024-09-17 17:09:42 +0100

    added variables.py - makes future editing of variables easier

---
+++ variables.py
@@ -0,0 +1,11 @@
+# A list of variables that depends on the nomenclature of the NBB.
+# This list exists for changing variables in the future i.c.o. changes
+
+address = 'Address'
+administrators = 'Administrators'
+enterprise_name = 'EnterpriseName'
+fin_data = 'Rubrics'
+joint_commities = 'JointCommittees'
+legal_form = 'LegalForm'
+participating_interests = 'ParticipatingInterests'
+shareholders = 'Shareholders'
