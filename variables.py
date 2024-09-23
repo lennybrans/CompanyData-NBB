0732d1da042500b934198daca73b20156bbd24f1
lennybrans <len_3000@outlook.com>
2024-09-23 13:04:27 +0100

    updated variables

--- variables.py
+++ variables.py
@@ -1,6 +1,12 @@
 # A list of variables that depends on the nomenclature of the NBB.
 # This list exists for changing variables in the future i.c.o. changes
 
+# reference data
+start_date = 'ExerciseDates.startDate'
+end_date = 'ExerciseDates.endDate'
+
+# financial statement data
+reference_id = 'ReferenceNumber'
 address = 'Address'
 administrators = 'Administrators'
 enterprise_name = 'EnterpriseName'
