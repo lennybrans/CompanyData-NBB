1b96f654578c2a3fd5c2897a835bab8a16d7ac6a
lennybrans <len_3000@outlook.com>
2024-10-07 17:57:15 +0100

    update example.py

--- example.py
+++ example.py
@@ -2,31 +2,33 @@ import CompanyData as cd
 import variables as var
 
 
-company =cd.CompanyData("BE 0473.416.418")
+company =cd.CompanyData("0428003392") # BE 0428.003.392
 
 # Reference API call
-company_references = company.fetch_references(year_span=2)
+company_references = company.fetch_references(year_span=1)
+print(company_references)
 
-# Data API call
-company_data = company.fetch_data(company_references)
+# # Data API call
+# company_data = company.fetch_data(company_references)
 
-# Get sub_keys without prior information
-company_ref_id = list(company_data.keys())
-print(company_ref_id)
+# # Get sub_keys without prior information
+# list_company_ref = list(company_data.keys())
+# print(list_company_ref)
 
-sub_keys = []
-for ref_id in company_ref_id:
-    sub_keys.extend(list(company_data[ref_id].keys()))
+# list_sub_keys = []
+# for reference in list_company_ref:
+#     list_sub_keys.extend(list(company_data[reference].keys()))
 
-sub_keys = list(set(sub_keys))
-print(sub_keys)
+# list_sub_keys = list(set(list_sub_keys))
+# print(list_sub_keys)
 
-# Variables
-current_name = company_data[company_ref_id[-1]][var.enterprise_name]
-current_address = company_data[company_ref_id[-1]][var.address]
-current_administators = company_data[company_ref_id[-1]][var.administrators]
-# To be extended
+# # Variables
+# current_name = company_data[list_company_ref[-1]][var.enterprise_name]
+# current_address = company_data[list_company_ref[-1]][var.address]
+# current_administators = company_data[list_company_ref[-1]][var.administrators]
+# # To be extended
 
-# DataFrame with financial data for requested years -> see fetch_references
-extract_data = cd.fetch_fin_data(company_data)
-print(extract_data)
+# # DataFrame with financial data for requested years -> see fetch_references
+# fin_data = cd.fetch_fin_data(company_data)
+# fin_data = fin_data.apply(cd.inventory_cycle_finished, axis=1)
+# print(fin_data.DIO_finished)
