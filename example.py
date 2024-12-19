3b23031dc83ad75332a2615acc566e64d918edbf
lennybrans <len_3000@outlook.com>
2024-12-19 12:16:53 +0000

    Updated example with Excel export

--- example.py
+++ example.py
@@ -3,7 +3,10 @@ import CompanyData as cd
 company =cd.CompanyData("0428.003.392") # BE 0428.003.392 or BE 0627.792.215
 
 # Reference API call
-company_references = company.fetch_references(year_span=2)
+company_references = company.fetch_references(year_span=1)
 company_dict = company.fetch_data(company_references)
-company_fin = cd.fetch_fin_data(company_dict, ['N', 'NM1'])
-print(company_dict)
+company_fin = cd.fetch_fin_data(company_dict, period=['N', 'NM1'])
+print(company_dict.keys())
+print(company_fin)
+
+cd.excel_export(company_dict, 'test.xlsx', period=['N'])
