577faa81bec26c25b682c02d2bf6fe02f3c409b1
lennybrans <len_3000@outlook.com>
2025-01-15 18:59:38 +0000

    Updated examples

--- example.py
+++ example.py
@@ -1,12 +1,9 @@
 import CompanyData as cd
 
-company =cd.CompanyData("0428.003.392") # BE 0428.003.392 or BE 0627.792.215
+company = cd.CompanyData("0428.003.392") # BE 0428.003.392 or BE 0627.792.215
 
-# Reference API call
-company_references = company.fetch_references(year_span=1)
-company_dict = company.fetch_data(company_references)
-company_fin = cd.fetch_fin_data(company_dict, period=['N', 'NM1'])
-print(company_dict.keys())
-print(company_fin)
-
-cd.excel_export(company_dict, 'test.xlsx', period=['N'])
+# Check company attributes such as company.data and company.reference_table
+# company.data is a dictionary that contains Filing objects
+# E.g. {'reference_number': Filing-class object}
+# The object contains all information, inlcuding metadata about the filing
+# Check the class for its attributes
--- example2.py
+++ example2.py
@@ -1,19 +1,3 @@
 import CompanyData as cd
 
-list_of_companies = ['0563455578','BE 0448.746.645', '0473416418']
-
-dictionary = {}
-
-for company in list_of_companies:
-    company_id = cd.CompanyData(company)
-    company_references = company_id.fetch_references(year_span=2)
-    company_data = company_id.fetch_data(company_references)
-    dictionary[company] = company_data
-
-print(dictionary.keys())
-
-dreamland = dictionary['BE 0448.746.645']
-telenet = dictionary['0473416418']
-
-dreamland_fin_data = cd.fetch_fin_data(dreamland, ['N','NM1'])
-print(dreamland_fin_data)
+# Temporary unavailable
